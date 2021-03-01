7e0b6a075677c02ebb5e4216e2276abe56c8942f
Mohil Khare <mohilkhare17@gmail.com>
2021-03-01 13:58:55 +0530

    working on alerts

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
     form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('layouts/home.html')
+        else:
+            form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
     

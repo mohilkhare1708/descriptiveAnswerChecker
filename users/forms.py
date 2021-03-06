fb4163f5765f92753fea45783536065bd994ee73
mohilkhare1708 <mohilkhare17@gmail.com>
2021-03-06 22:16:47 +0530

    perfected the registration form look

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
--- users/views.py
+++ users/views.py
@@ -4,15 +4,14 @@ from django.contrib import messages
 
 
 def register(request):
-    form = UserRegisterForm()
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('home-page')
-        else:
-            form = UserRegisterForm()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
     

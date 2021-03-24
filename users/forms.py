3533b3559b4b6a1bd0ec6400f84bd227d56259a4
mohilkhare1708 <mohilkhare17@gmail.com>
2021-03-24 18:09:08 +0530

    adding the fullname field to the register form

--- users/forms.py
+++ users/forms.py
@@ -4,11 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    full_name = forms.CharField(required=True)
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'email', 'password1', 'password2']:
+        for fieldname in ['username', 'email', 'full_name', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
             
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'full_name', 'password1', 'password2']

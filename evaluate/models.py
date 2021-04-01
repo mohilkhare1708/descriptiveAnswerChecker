88f628f18403eb9eecbb3cb6e872d59295520ddb
mohilkhare1708 <mohilkhare17@gmail.com>
2021-04-01 22:56:11 +0530

    db discuss

--- evaluate/models.py
+++ evaluate/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 class Test(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +11,9 @@ class Test(models.Model):
     total_marks = models.IntegerField()
     passing_marks = models.IntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+# class Result(models.Model):
+#     test = models.OneToOneField(Test, on_delete=models.CASCADE)
+#     token = models.CharField(default='')
+#     result = models.FileField( upload_to=None, max_length=100)
+    
--- evaluate/views.py
+++ evaluate/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from evaluate.forms import TestCreateForm
+from evaluate.models import Result
 
 # Create your views here.
 @login_required
@@ -13,6 +13,8 @@ def createTest(request):
             doc = form.save(commit=False)
             doc.user = request.user
             doc.save()
+            # res = Result(test = doc.id, result = '/results/test.csv')
+            # res.save()
             return redirect('home-page')
     else:
         form = TestCreateForm()

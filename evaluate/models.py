5c81b76185374a114b823b098f7e17fba7c71c67
Harsh <42941023+hkatkade@users.noreply.github.com>
2021-04-05 22:06:03 +0530

    marks generation

--- evaluate/models.py
+++ evaluate/models.py
@@ -6,7 +6,15 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 import pandas as pd
 import os
+import string
+import random
 from django.conf import settings
+from attributecheck import tfLCSChecker, cleanSentence, keywordsChecker, lengthChecker
+
+a=0.2
+b = 0.5
+c = 0.3
+N = 5
 
 class Test(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -42,14 +50,26 @@ def update_result_signal(sender, instance, created, **kwargs):
         mans= pd.DataFrame(data=mans)
         rsheet = pd.read_csv(os.path.join(settings.BASE_DIR, str(instance.response_sheet)))
         rsheet= pd.DataFrame(data=rsheet)
-        names, emails, scores = [], [], []
+        names, emails, scores, rollno, answer = [], [], [], [], []
         for name in rsheet['Name']:
             names.append(name)
         for email in rsheet['Email']:
             emails.append(email)
+        for roll in rsheet['Roll Number']:
+            rollno.append(roll)
+        for ans in rsheet['Answers']:
+            answer.append(ans)
         # score generation logic here
-        for i in range(len(names)):
-            scores.append(20)
+        for i in range(len(answer)):
+            cleaned = cleanSentence(answer[i])
+            lCS_score = tfLCSChecker(cleaned)
+            len_score = lengthChecker(cleaned)
+            keyword_score = keywordsChecker(cleaned)
+            marks = a * lCS_score + b * len_score + c * keyword_score
+            scores.append(marks)
+            df = pd.DataFrame(list(zip(rollno, names, score, emails)), columns=['Roll Number', 'Name', 'Marks', 'Email'])
+            token = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
+            df.to_csv(f'{token}.csv')
         successRate, totalStudents = 0, len(names)
         meanPercentage = (sum(scores)/(instance.total_marks*totalStudents))*100
         highScore = max(scores)
